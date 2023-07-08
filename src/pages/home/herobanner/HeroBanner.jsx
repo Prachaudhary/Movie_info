6f62c56f03b13133e2ada6c23b80d228a22c6c76
pradhuman chaudhary <pradhumanchoudhary13@gmail.com>
2023-07-08 23:27:17 +0530

    first commit

--- src/pages/home/herobanner/HeroBanner.jsx
+++ src/pages/home/herobanner/HeroBanner.jsx
@@ -39,7 +39,7 @@ const HeroBanner = () => {
 
       <ContentWrapper>
         <div className="heroBannerContent">
-          <span className="title">Welcome..</span>
+          <span className="title">Welcome....</span>
           <span className="subTitle">
             Millions of movies, TV shows and people to discover.. Explore now..
           </span>
